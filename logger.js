b6c379fb72195ddf429a5171e51a6a59bac00731
Facundo Crego <facundo.crego@hinch.as>
2015-07-27 13:31:17 -0300

    First commit

---
+++ logger.js
@@ -0,0 +1,111 @@
+var filename = "./console.log";
+
+var spawn = require('child_process').spawn;
+var async = require('async');
+var fs = require('fs');
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var port = process.env.PORT || 3003;
+
+var os=require('os');
+var ifaces=os.networkInterfaces();
+var ipAddress = null;
+
+function getIp(details){
+    if (details.family === 'IPv4') {
+      ipAddress = details.address;
+    }
+}
+
+for (var dev in ifaces) {
+    if(dev !== 'eth0') {
+        continue;
+    }
+    ifaces[dev].forEach(getIp);
+}
+
+console.log('IP: '+ipAddress);
+
+server.listen(port, function () {
+  console.log('Server listening at port %d', port);
+  console.log('Append this code in the client:');
+  console.log('<script id="logger" src="http://'+ipAddress+':3003/console-log.js"></script>');
+  console.log('Go to http://facka.github.io/logger-web/ to connect to this logger');
+});
+
+app.use(express.static(__dirname + '/public/'));
+
+var connections = {};
+var logReaders = {};
+
+io.on('connection', function (socket) {
+
+    console.log('Connected new socket');
+
+    //logData = { clientId: '23424sdfsdfs4', message: 'abcdef'}
+    socket.on('log', function (logData) {
+
+        if (!fs.existsSync('./' + logData.clientId)){
+            fs.mkdirSync('./' + logData.clientId);
+            connections[socket.id] = logData.clientId;
+        }
+
+        var filename = './' + logData.clientId + '/console.log';
+        fs.appendFile(filename, logData.message);
+    });
+
+    socket.on('checkfile', function (data) {
+        console.log(JSON.stringify(data));
+        var filename = './' + (data.path ?  data.path + '/' : '') + data.file;
+        console.log('Checking file: ' + filename);
+        fs.exists(filename, function(exists) {
+            if (exists) {
+                console.log('Emit ready!');
+                socket.emit('ready', filename);
+            } else {
+                console.log('file not found.');
+                socket.emit('logger_error', 'FILE_NOT_FOUND');
+            }
+        });
+    });
+
+    socket.on('start', function (filename) {
+        console.log('starting logger');
+        var tailProcess = spawn('tail', ['-f', filename]);
+        tailProcess.stdout.on('data', function (data) {
+          console.log('sending data...' + data);
+          socket.emit('data', ''+data);
+        });
+        logReaders[socket.id] = tailProcess;
+    });
+
+    socket.on('pause', function () {
+        console.log('Killing tail process');
+        var tailProcess = logReaders[socket.id];
+        if (tailProcess) {
+            tailProcess.kill();
+            tailProcess = null;
+        }
+    });
+
+    socket.on('disconnect', function () {
+        var dir = connections[socket.id];
+        //Delete folder
+        if (dir) {
+            fs.unlinkSync(dir + '/console.log');
+            fs.rmdir(dir);
+        }
+        var tailProcess = logReaders[socket.id];
+        console.log('Killing tail process');
+        if (tailProcess) {
+            tailProcess.kill();
+            tailProcess = null;
+            delete logReaders[socket.id];
+        }
+
+    });
+
+});
+
---
+++ public/main.js
@@ -0,0 +1,257 @@
+angular.module('loggerApp', ['mgcrea.ngStrap','ngAnimate','ngStorage'])
+  .controller('PanelContainerController', ['PanelsService', '$scope', function(PanelsService, $scope) {
+    var container = this;
+    container.panels = PanelsService.panels;
+
+    container.close = function(id) {
+        PanelsService.removePanel(id);
+    }
+  }]);
+
+angular.module('loggerApp').controller('HeaderController', ['$scope','PanelsService', '$modal', function($scope, PanelsService, $modal) {
+  var header = this;
+
+  $scope.config = {};
+  $scope.save = createPanel;
+  $scope.errors = [];
+
+  var newPanelModal = $modal({scope: $scope, template: 'views/newPanelModal.html', show: false});
+
+  function createPanel(cancel) {
+        $scope.errors.length = 0;
+        PanelsService.createPanel($scope.config).then(function(panel){
+            PanelsService.addPanel(panel);
+            newPanelModal.hide();
+        },function(error) {
+            if (error === 'FILE_NOT_FOUND') {
+                $scope.errors.push('File not found.');
+            }
+            else {
+                $scope.errors.push('Soemthing went wrong. Please, check the info that you send.');
+            }
+        });
+  }
+
+  header.openNewPanelModal = function() {
+      $scope.config = {};
+      newPanelModal.$promise.then(newPanelModal.show);
+  };
+}]);
+
+angular.module('loggerApp').directive('highlight', function() {
+    return {
+        restrict: 'A',
+        scope: {
+            highlight: '='
+        },
+        link: function (scope, element) {
+            var value = element[0].innerHTML.trim();
+            var message = scope.$parent.$eval(value.substring(2, value.length - 2));;
+
+            scope.$watch(function() {
+                return scope.highlight;
+            }, function(newVal) {
+                if (newVal) {
+                    var highlightedMessage = message.split(newVal).join('<mark>'+newVal+'</mark>');
+                    element[0].innerHTML = highlightedMessage;
+                }
+                else {
+                    element[0].innerHTML = message;
+                }
+            })
+        }
+    };
+});
+
+angular.module('loggerApp').directive('scrollBottom', function() {
+    return {
+        restrict: 'A',
+        scope: {
+            scrollBottom: '='
+        },
+        link: function (scope, element) {
+            scope.scrollBottom(function() {
+                element[0].scrollTop = element[0].scrollHeight;
+            });
+        }
+    };
+});
+
+angular.module('loggerApp').service('PanelsService', ['Panel','$q','$localStorage', function(Panel, $q, $localStorage) {
+        var _this = this;
+        this.panels = {};
+
+        function save(panelConfig) {
+            var store = [];
+            if ($localStorage.panels) {
+                store = JSON.parse($localStorage.panels);
+            }
+            store.push(panelConfig);
+            $localStorage.panels = JSON.stringify(store);
+        }
+
+        function load() {
+            if ($localStorage.panels) {
+                var panels = JSON.parse($localStorage.panels);
+                panels.forEach(function(panelConfig) {
+                    _this.createPanel(panelConfig).then(function(panel) {
+                        _this.panels[panel.id] = panel;
+                    });
+                });
+            }
+        }
+
+        this.createPanel = function(config) {
+            return $q(function(resolve, reject){
+                var panel = new Panel(config.host, config.port, config.path, config.file);
+                panel.onReady(function() {
+                    resolve(panel);
+                });
+                panel.onError(function(error) {
+                    reject(error);
+                });
+            });
+        }
+
+        this.addPanel = function(panel) {
+            this.panels[panel.id] = panel;
+            save(panel.config);
+        };
+
+        this.removePanel = function(id) {
+            if (this.panels[id]) {
+                this.panels[id].close();
+                delete this.panels[id];
+            }
+        };
+
+        load();
+}]);
+
+angular.module('loggerApp').factory('Panel', function($timeout) {
+    var PLAYING = 'PLAYING';
+    var PAUSED = 'PAUSED';
+
+    var id = 0;
+
+    var Panel = function(host, port, path, file) {
+        var _this = this;
+        this.id = id++;
+        this.log = [];
+        this.size = 10000;
+        this.filteredLog = [];
+        this.status = PLAYING;
+        this.config = {
+            host: host,
+            port: port,
+            path: path,
+            file: file,
+            live: true,
+            date: new Date(),
+            dateFormat: 'mm/dd/YYYY'
+        };
+        this.searchText = '';
+        this.socket = io('http://' + this.config.host + ':' + this.config.port, {forceNew: true});
+
+        this.socket.on('logger_error', function (error) {
+            _this.notifyError(error);
+        });
+
+        this.socket.on("connect_failed",function() {
+            _this.notifyError('CONNECT_FAILED');
+        });
+
+        this.socket.on("connect_error",function() {
+            _this.notifyError('CONNECT_ERROR');
+            _this.socket.io.disconnect();
+        });
+
+        this.socket.on('ready', function (filename) {
+            _this.socket.emit('start', filename);
+            _this.notifyReady();
+        });
+
+        this.socket.on('data', function (data) {
+            $timeout(function() {
+                var lines = data.split('\n').filter(function(value) {
+                    return !!value;
+                });
+                var array = [];
+                lines.forEach(function (line) {
+                    array.push({
+                        value: line,
+                        id: _this.log.length + array.length
+                    });
+                });
+                // Checks if new lines exceed log size
+                if (_this.log.length + array.length > _this.size) {
+                    var removeCount = (_this.log.length + array.length) - _this.size;
+                    for (var i=0; i< removeCount; i++) {
+                        _this.log.shift();
+                    }
+                }
+                _this.log = _this.log.concat(array);
+                _this.refresh();
+                _this.scrollBottom();
+            },0);
+        });
+    };
+
+    Panel.prototype = {
+        addLines: function(lines) {
+            this.log = this.log.concat(lines);
+            this.refresh();
+        },
+        play: function() {
+            this.status = 'PLAYING';
+            this.log = [];
+            this.socket.emit('start', './' + (this.config.path ?  this.config.path + '/' : '') + this.config.file);
+        },
+        pause: function() {
+            this.status = 'PAUSED';
+            this.socket.emit('pause');
+        },
+        close: function() {
+            this.socket.io.disconnect();
+        },
+        refresh: function() {
+            var searchText = this.searchText;
+            if (searchText) {
+                this.filteredLog = this.log.filter(function(line){
+                    return line.value.indexOf(searchText) !== -1;
+                });
+            }
+            else {
+                this.filteredLog = angular.copy(this.log);
+            }
+        },
+        setFile: function(live, date) {
+            if (live) {
+                this.file = this.config.file;
+            }
+            else {
+                this.file = this.config.file + '-' + this.date;
+            }
+        },
+        onReady: function(callback) {
+            this.socket.emit('checkfile', {
+                path: this.config.path,
+                file: this.config.file
+            });
+            this.notifyReady = callback;
+        },
+        onError: function(callback) {
+            this.notifyError = callback;
+        },
+        setScrollBottom: function(func) {
+            this.$parent.panel.notifyScrollBottom = func;
+        },
+        scrollBottom: function() {
+            if (this.notifyScrollBottom) {
+                this.notifyScrollBottom();
+            }
+        }
+    };
+
+    return Panel;
+});
